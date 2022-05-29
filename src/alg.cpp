8747d30e48e6ea87c4e1b09478495333f2d2555b
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-05-29 13:42:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,43 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+final int HigherCase = 1;
+final int LowerCase = 0;
+final int NotLetter = 2;
+
+void lowerOrHigherCase(char symb) {
+  int SymbAscII = symb;
+  if (symb > 64 && symb < 91)
+    return HigherCase;
+  if (symb > 96 && symb < 123)
+    return LowerCase;
+  return NotLetter;
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> stringTree;
+  std::ifstream file(filename);
+  if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+  
+  std::string TextToAdd = "";
+  while(!file.eof()) {
+    char SymbToAdd = file.get();
+    
+    switch (lowerOrHigherCase(SymbToAdd)) {
+      case : HigherCase : {
+        TextToAdd += toLower(SymbToAdd);
+      } break;
+      case : LowerCase : {
+        TextToAdd += SymbToAdd;
+      } break;
+      case : NotLetter : {
+        stringTree.add(TextToAdd);
+      } break;
+    }
+  }
+file.close();
+return stringTree;
 }
